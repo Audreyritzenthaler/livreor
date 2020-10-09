97be45140e2936b168952511e524abf3900f470e
ritzenthaler.audrey@gmail.com <ritzenthaler.audrey@gmail.com>
2020-10-09 17:26:24 +0200

    create post route

---
+++ back/routes/comments.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const router = express.Router()
+const connection = require('../database')
+
+router.get('/', (req, res) => {
+  connection.query('SELECT * FROM comments', (err, result) => {
+    if (err) {
+      return res.status(500).json({ error: 'Internal server error' })
+    } else {
+      return res.status(200).json(result)
+    }
+  })
+})
+
+router.post('/', (req, res) => {
+  if (req.body.message) {
+    const comment = req.body
+
+    connection.query('INSERT INTO comments SET ?', comment, (err, result) => {
+      if (err) {
+        return res.status(500).json({ error: 'Internal server error !' })
+      } else {
+        return res.status(201).json({ response: 'Bien envoyé !' })
+      }
+    })
+  } else {
+    return res.status(500).json({ error: 'Write a message please !' })
+  }
+
+})
+
+module.exports = router
