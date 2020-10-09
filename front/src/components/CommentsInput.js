2423e97a68308577d0ce82d74a03c0e462e85c3e
ritzenthaler.audrey@gmail.com <ritzenthaler.audrey@gmail.com>
2020-10-09 18:11:17 +0200

    create submit function to submit a message

--- front/src/components/CommentsInput.js
+++ front/src/components/CommentsInput.js
@@ -1,15 +1,37 @@
 import React, { useState } from 'react'
+import axios from 'axios'
+
 import './CommentsInput.css'
 
-const CommentsInput = ({ submit }) => {
-  const [comment, setComment] = useState('')
+const CommentsInput = () => {
+  const [message, setMessage] = useState('')
+
+  const submit = (e) => {
+    e.preventDefault()
+    axios({
+      method: 'post',
+      url: 'http://localhost:8080/comments',
+      data: {
+        message
+      }
+    })
+      .then((reponse) => {
+        //On traite la suite une fois la réponse obtenue 
+        console.log(reponse);
+      })
+      .catch((erreur) => {
+        //On traite ici les erreurs éventuellement survenues
+        console.log(erreur);
+      })
+
+  }
 
   return (
     <form onSubmit={submit}>
       <textarea
         className='inputComment'
-        value={comment}
-        onChange={e => setComment(e.target.value)}
+        value={message}
+        onChange={e => setMessage(e.target.value)}
         placeholder=' Write your comment here !'
         rows="5"
         cols="33">
