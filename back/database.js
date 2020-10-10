204a762abb6082e6699b728ddf1fae31007da43d
ritzenthaler.audrey@gmail.com <ritzenthaler.audrey@gmail.com>
2020-10-10 10:38:48 +0200

    use process.env.CLEARDB_DATABASE_URL

--- back/database.js
+++ back/database.js
@@ -1,12 +1,16 @@
 const mysql = require('mysql')
 require('dotenv').config()
 
-const connection = mysql.createConnection({
+const dbSettings = typeof process.env.CLEARDB_DATABASE_URL === 'string' ? process.env.CLEARDB_DATABASE_URL : {
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME
-})
+} 
+
+const connection = mysql.createConnection(
+  dbSettings
+)
 
 connection.connect((err) => {
   if (err) throw err
